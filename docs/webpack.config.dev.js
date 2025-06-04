2aceb4e06205ac0435c97b5c88b6512a58aa76b1
Dennis Jen <dsjen@broadinstitute.org>
2025-06-04 10:06:51 -0400

    Upgraded webpack

--- docs/webpack.config.dev.js
+++ docs/webpack.config.dev.js
@@ -1,25 +1,36 @@
-const path = require('path');
-const filename = 'homepage.webpack.min.js';
-const library = 'homepage';
+const path = require("path");
+
+const filename = "homepage.webpack.min.js";
+const library = "homepage";
 
 module.exports = {
-    target: "web",
-    entry: './src/js/index.js',
-    devtool: 'inline-source-map',
-    output: {
-        filename: filename,
-        path: path.resolve(__dirname, 'dist/js'),
-        libraryTarget: 'var',
-        library: library,
+  target: "web",
+  mode: "development",
+  entry: "./src/js/index.js",
+  devtool: "inline-source-map",
+
+  output: {
+    filename,
+    path: path.resolve(__dirname, "dist/js"),
+    libraryTarget: "var",
+    library,
+    hashFunction: "xxhash64",
+  },
+
+  performance: {
+    hints: false,
+  },
+
+  devServer: {
+    port: 8000,
+    static: {
+      directory: path.resolve(__dirname, "./"),
+      watch: true,
     },
-    devServer: {
-        port: 8000,
-        publicPath: '/dist/js',
-        contentBase: path.resolve(__dirname, "./"),
-        hot: true,
-        watchContentBase: true,
-        stats: {
-            children: false
-        }
-    }
+    devMiddleware: {
+      publicPath: "/dist/js",
+      stats: "errors-warnings",
+    },
+    hot: true,
+  },
 };
