0c6ce06b98457e128c9e9b00c24c59bd5b579940
Dennis Jen <dsjen@broadinstitute.org>
2025-12-12 10:11:58 -0500

    upgrade storybook

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,7 +1,7 @@
 /** @type { import('@storybook/react-vite').StorybookConfig } */
 const config = {
   stories: ["../stories/**/*.stories.@(js|jsx|ts|tsx|mdx)"],
-  addons: ["@storybook/addon-essentials"],
+  addons: ["@storybook/addon-docs"],
   framework: {
     name: "@storybook/react-vite",
     options: {},
